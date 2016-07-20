99ffea9ba376ee7c79a6cb6aec508d49a3654b8c
linofex <alessandro.noferi@gmail.com>
2016-07-20 17:32:29 +0200

    Addded Deck.cc

---
+++ Deck.cc
@@ -0,0 +1,17 @@
+#include "Deck.h"
+#include <algorithm>
+#include <ctime>        // for time()
+#include <cstdlib>      // std::rand, std::srand
+
+void Deck::Shuffle(){
+	std::srand (unsigned (std::time(0)));
+	random_shuffle(deck.begin(), deck.end());
+}
+	
+bool Deck::Empty(){
+	return deck.size() == 0;
+}
+
+
+
+
