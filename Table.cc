b8bac2acf197c9a94a8b5ca826cb2552fbcbd97a
linofex <alessandro.noferi@gmail.com>
2016-07-26 15:09:45 +0200

    Added Table

---
+++ Table.cc
@@ -0,0 +1,12 @@
+#include "Table.h"
+
+Table::PrintTable(){
+	m_map::const_iterator iter = table.begin();
+	for(; iter != table.end(); ++iter){
+		std::cout<< iter->first;
+		for(int i = 0; i<iter->second.size(): ++i){
+			std::cout<<iter-second[i] << " ";
+		}
+		std::cout<<std::endl;
+	}
+}
---
+++ Table.h
@@ -0,0 +1,20 @@
+ #ifndef TABLE_H
+#define TABLE_H
+
+#include <map>
+#include <vector>
+
+class Table{
+	private:
+		typedef std::map<int, std::vector<std::string>> m_map:
+		m_map table;
+		int n_set; //Optional
+	public:
+		void PrintTable(){};
+		inline void UpdateTable(const m_map& table_){ 
+			table = table_;
+		}
+};
+
+#endif
+
