15eb305ba40fd5ca43bc5334764726d8d1d5dab3
linofex <alessandro.noferi@gmail.com>
2016-09-06 19:00:30 +0200

    Improved main.cc

--- main.cc
+++ main.cc
@@ -7,10 +7,11 @@ int main(){
 	int num;
 	std::cout << "Inserisci il numero di giocatori: ";
 	std::cin >> num;
+	std::cin.ignore();
 	for(int i = 0 ; i < num ; ++i){
 		std::string name;
-		std::cout << "Inserire il numero del giocatore " << i << std::endl;
-		std::cin >>  name;
+		std::cout << "Inserire il nome del giocatore " << i << std::endl;
+		getline(std::cin >> std::skipws, name);
 		players.push_back(new PlayerBase(name));
 	}	
 	Machiavelli Game(players, deck, table);
