2b1c91829751d8b100efc77cdcb460b98c933f44
linofex <alessandro.noferi@gmail.com>
2016-09-29 21:50:28 +0200

    Update Machiavelli.h

--- Machiavelli.h
+++ Machiavelli.h
@@ -33,7 +33,7 @@ class Machiavelli{
 		void DealCards();
 		
 		// Metodo che esegue la mossa, prendi in input il giocatore i-esimo
-		bool Move(const int i);
+		bool Move(const int& i);
 		
 		// Metodo che esegue il gioco.
 		void Game();
